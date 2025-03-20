bf1417912cb703d82e56f6a8318ae5b2a01ebe2e
Nikitahh <nikitahsieh03@gmail.com>
2025-03-20 20:53:33 +0800

    add mobile side menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,34 @@
 import '../scss/styles.scss'
+
+const btnOpen = document.querySelector('#btnOpen')
+const btnClose = document.querySelector('#btnClose')
+const media = window.matchMedia('(max-width: 1110px)')
+const navContent = document.querySelector('.nav__content')
+const navOverlay = document.querySelector('.nav__overlay ')
+
+function openMobileMenu() {
+	btnOpen.setAttribute('aria-expanded', 'true')
+}
+
+function closeMobileMenu() {
+	btnOpen.setAttribute('aria-expanded', 'false')
+}
+
+function setupNav(e) {
+	if (e.matches) {
+		setTimeout(() => {
+			navContent.style.display = 'block'
+			navOverlay.style.display = 'block'
+		}, 500)
+	} else {
+	}
+}
+
+setupNav(media)
+
+btnOpen.addEventListener('click', openMobileMenu)
+btnClose.addEventListener('click', closeMobileMenu)
+
+media.addEventListener('change', function (e) {
+	setupNav(e)
+})
